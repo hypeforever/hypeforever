f33d53c18fce454f524b50ee513ef0c6730e4e52
theluckyluke <luke.httq@gmail.com>
2017-02-13 22:29:44 +0100

    Create SnapToGrid script

---
+++ Assets/Scripts/SnapToGrid.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+// Source: http://www.alanzucconi.com/2015/07/22/how-to-snap-to-grid-in-unity3d/
+
+// This script is executed in the editor
+[ExecuteInEditMode]
+public class SnapToGrid : MonoBehaviour {
+#if UNITY_EDITOR
+	public float snapValue = 0.5f;
+
+	void Update () {
+		if (snapValue > 0.0f) {
+			transform.localPosition = RoundTransform(transform.localPosition, snapValue);
+		}
+	}
+
+	// The snapping code
+	private Vector3 RoundTransform(Vector3 v, float snapValue) {
+		return new Vector3 (
+			snapValue * Mathf.RoundToInt(v.x / snapValue),
+			snapValue * Mathf.RoundToInt(v.y / snapValue),
+			snapValue * Mathf.RoundToInt(v.z / snapValue)
+		);
+	}
+#endif
+}
