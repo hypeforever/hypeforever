5f50d56c998c71340ef812a457f9f27387bf0ef9
theluckyluke <luke.httq@gmail.com>
2017-07-14 10:59:00 +0200

    Improve SnapToGrid script

--- Assets/Scripts/SnapToGrid.cs
+++ Assets/Scripts/SnapToGrid.cs
@@ -1,26 +1,45 @@
 ﻿using UnityEngine;
 
-// Source: http://www.alanzucconi.com/2015/07/22/how-to-snap-to-grid-in-unity3d/
+// Inspired by: http://www.alanzucconi.com/2015/07/22/how-to-snap-to-grid-in-unity3d/
 
 // This script is executed in the editor
 [ExecuteInEditMode]
 public class SnapToGrid : MonoBehaviour {
 #if UNITY_EDITOR
-	public float snapValue = 0.5f;
+	public bool snapPosition = true;
+	public float snapPositionValue = 0.5f;
+
+	public bool snapRotation = true;
+	public float snapRotateValue = 90f;
 
 	void Update () {
-		if (snapValue > 0.0f) {
-			transform.localPosition = RoundTransform(transform.localPosition, snapValue);
+		if (snapPosition && snapPositionValue > 0.0f) {
+			transform.localPosition = calculateSnapPosition(transform.localPosition);
+		}
+
+		if (snapRotation && snapRotateValue > 0.0f) {
+			transform.eulerAngles = calculateSnapRotation(transform.eulerAngles);
 		}
 	}
 
-	// The snapping code
-	private Vector3 RoundTransform(Vector3 v, float snapValue) {
+	private Vector3 calculateSnapPosition(Vector3 v) {
 		return new Vector3 (
-			snapValue * Mathf.RoundToInt(v.x / snapValue),
-			snapValue * Mathf.RoundToInt(v.y / snapValue),
-			snapValue * Mathf.RoundToInt(v.z / snapValue)
+			round(v.x, snapPositionValue),
+			round(v.y, snapPositionValue),
+			round(v.z, snapPositionValue)
 		);
+	}
+	
+	private Vector3 calculateSnapRotation(Vector3 eulerAngles) {
+		return new Vector3 (
+			round(eulerAngles.x, snapRotateValue),
+			round(eulerAngles.y, snapRotateValue),
+			round(eulerAngles.z, snapRotateValue)
+		);
+	}
+
+	private float round(float angle, float snapValue) {
+		return snapValue * Mathf.RoundToInt(angle / snapValue);
 	}
 #endif
 }
