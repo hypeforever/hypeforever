d745d478ec7d3524749d32599a0cd7ce78c874aa
theluckyluke <luke.httq@gmail.com>
2017-12-30 13:06:19 +0100

    Improve ThirdPersonCamera

--- Assets/Animation/ThirdPersonCamera.cs
+++ Assets/Animation/ThirdPersonCamera.cs
@@ -2,16 +2,17 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ThirdPersonCamera : MonoBehaviour {
-
+public class ThirdPersonCamera : MonoBehaviour
+{
 	public float mouseSensitivity = 10;
 	public Transform target;
 	public float distanceFromTarget = 2;
 	public Vector2 pitchBounds = new Vector2 (-40, 85);
 
 	public float rotationSmoothTime = 0.12f;
+	public float sphereCastThickness = 0.2f;
 
-	public bool lockCursor = true;
+	public bool cursorLocked = true;
 
 	public LayerMask CamOcclusion;
 
@@ -21,38 +22,59 @@ public class ThirdPersonCamera : MonoBehaviour {
 	private float yaw;
 	private float pitch;
 
-	// Use this for initialization
-	void Start () {
-		if (lockCursor) {
+	public void Start()
+	{
+		lockCursor();
+	}
+
+	private void lockCursor()
+	{
+		if (cursorLocked)
+		{
 			Cursor.lockState = CursorLockMode.Locked;
 			Cursor.visible = false;
 		}
 	}
 	
-	// Update is called once per frame
-	void LateUpdate () {
-		yaw += Input.GetAxis ("Mouse X") * mouseSensitivity;
-		pitch -= Input.GetAxis ("Mouse Y") * mouseSensitivity;
+	public void LateUpdate()
+	{
+		handleRotation();
+		calculatePosition();
+	}
+
+	private void handleRotation()
+	{
+		yaw += Input.GetAxis("Mouse X") * mouseSensitivity;
+		pitch -= Input.GetAxis("Mouse Y") * mouseSensitivity;
 		pitch = Mathf.Clamp (pitch, pitchBounds.x, pitchBounds.y);
 
 		currentRotation = Vector3.SmoothDamp(currentRotation, new Vector3(pitch, yaw), ref rotationSmoothVelocity, rotationSmoothTime);
 		transform.eulerAngles = currentRotation;
-
-		transform.position = target.position - transform.forward * distanceFromTarget;
-
-		transform.position = raycast ();
-		//transform.LookAt (target);
 	}
 
-	private Vector3 raycast()
+	private void calculatePosition()
 	{
-		RaycastHit hit = new RaycastHit ();
+		transform.position = getCameraPosition(getUnobstructedDistanceFromTargetToCamera());
+	}
 
-		if (Physics.Linecast (target.position, transform.position, out hit, CamOcclusion))
+	private Vector3 getCameraPosition(float distance)
+	{
+		return target.position + (getDirectionFromTargetToCamera() * distance);
+	}
+
+	private Vector3 getDirectionFromTargetToCamera()
+	{
+		return transform.forward * -1;
+	}
+
+	private float getUnobstructedDistanceFromTargetToCamera()
+	{
+		RaycastHit hit = new RaycastHit();
+		if (Physics.SphereCast(target.position, sphereCastThickness, getDirectionFromTargetToCamera(), out hit, distanceFromTarget, CamOcclusion))
 		{
-			return hit.point + hit.normal * 0.1f;
+			return hit.distance;
 		}
 
-		return transform.position;
+		return distanceFromTarget;
 	}
 }
